9124f01bc91dd0cd3f0878ae010a58a6334f59b1
jinah adam <jinahadam@gmail.com>
2013-05-16 23:37:57 +0800

    yaml :

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PgRocks::Application.config.secret_token = '1dfb387fa1553fb6851cf9d68d40a4b612378c76223bd0a7479fe644a41ab5f0b1209b554300960de47fca337b0b07fadd4f161aedf2621ae3d0706df2408910'
